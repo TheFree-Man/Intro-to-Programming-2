a896fb47a34768e7696d186344d2df21701d7154
Mark Gilmartin <S00198957@mail.itsligo.ie>
2020-02-21 10:36:30 +0000

    Bruh

---
+++ Lab 8/Q2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+
+            Console.Write("Enter characters: ");
+            inputString = Console.ReadLine();
+            Console.WriteLine();
+
+            printCharacters(inputString);
+        }
+
+        static void printCharacters(string inputString)
+        {
+            for (int i = 2; i < inputString.Length; i += 3)
+            {
+                Console.WriteLine("{0}", inputString[i]);
+            }
+
+        }
+
+    }
+}
