708238a657bd0d8fc552bb133b33b75142d6eb11
Mark Gilmartin <S00198957@mail.itsligo.ie>
2020-02-21 10:20:57 +0000

    Bruh

--- Lab 8/Q1/Program.cs
+++ Lab 8/Q1/Program.cs
@@ -6,6 +6,21 @@ namespace Q1
     {
         static void Main(string[] args)
         {
+            string inputString;
+
+            Console.Write("Enter characters: ");
+            inputString = Console.ReadLine();
+            Console.WriteLine();
+
+            printCharacters(inputString);
+        }
+
+        static void printCharacters(string inputString)
+        {
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                Console.WriteLine("{0}", inputString[i]);
+            } 
 
         }
     }
