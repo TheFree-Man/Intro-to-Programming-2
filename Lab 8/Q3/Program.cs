493b723c5a73a8b71a5c6e966ae236141b3b4dc2
Mark Gilmartin <S00198957@mail.itsligo.ie>
2020-02-21 10:53:38 +0000

    Bruh

--- Lab 8/Q3/Program.cs
+++ Lab 8/Q3/Program.cs
@@ -6,7 +6,32 @@ namespace Q3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int counter;
+            string inputString;
+
+            Console.Write("Enter characters: ");
+            inputString = Console.ReadLine();
+            Console.WriteLine();
+
+            counter = printCharacters(inputString);
+
+            Console.WriteLine("amount of X & Y = {0}", counter);
+        }
+
+        static int printCharacters(string inputString)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == 'x' || inputString[i] == 'y')
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+
         }
     }
 }
