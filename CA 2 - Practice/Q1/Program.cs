006dfaf59b2fef188fa2b754df93f54cafe0ed2c
Mark Gilmartin <S00198957@mail.itsligo.ie>
2020-02-28 10:34:58 +0000

    Quicksave

--- CA 2 - Practice/Q1/Program.cs
+++ CA 2 - Practice/Q1/Program.cs
@@ -4,9 +4,24 @@ namespace Q1
 {
     class Program
     {
+        static string[] gradeName = new string[] { "F", "E", "D", "C", "B", "A" };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i;
+            double studentMark;
+
+            for (i= 0; i != -999;)
+            {
+                Console.Write("Enter student mark: ");
+                studentMark = double.Parse(Console.ReadLine());
+                getGradeBand(studentMark);
+            }
+        }
+
+        static string getGradeBand(double studentMark)
+        {
+
         }
     }
 }
