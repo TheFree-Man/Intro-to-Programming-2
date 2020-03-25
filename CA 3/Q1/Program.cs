41ff26e72f351a628b39ab6b2c02429bf5429dd9
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2020-03-25 17:55:21 +0000

    Quicksave

--- CA 3/Q1/Program.cs
+++ CA 3/Q1/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Description: Programme that lets a user view player reports, 
+ *              location analysis reports, and search for a player.
+ *              
+ * Author     : Mark Gilmartin
+ * 
+ * Date       : 
+*/
+using System;
 using System.IO;
 
 namespace Q1
@@ -7,7 +15,21 @@ namespace Q1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            int userChoice = 0;
+            do
+            {
+                    Console.WriteLine("\nMenu\n");
+                    Console.WriteLine("1. Player Report");
+                    Console.WriteLine("2. Location Analysis Report");
+                    Console.WriteLine("3. Search for a player");
+                    Console.WriteLine("4. Exit");
+
+            } while (userChoice! < 1 && userChoice! > 4);
         }
     }
 }
