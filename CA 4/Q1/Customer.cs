e88e59f78f5bb3a011b7a286c2dcb3a067c381f0
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2020-04-20 19:48:09 +0100

    Quicksave

---
+++ CA 4/Q1/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q1
+{
+    class Customer
+    {
+        private int customerID = 0;
+        private string name;
+        private double accountBalance;
+
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public double AccountBalance
+        {
+            get
+            {
+                return accountBalance;
+            }
+            set
+            {
+                accountBalance = value;
+            }
+        }
+
+        public Customer()
+        {
+            customerID = DHCPserver();
+
+            accountBalance = 0;
+        }
+
+        public int DHCPserver()
+        {
+            customerID++;
+            return customerID;
+        }
+
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
