806fdce61e0c63ffc262526128f1d96fe086dcde
TheFree-Man <60262008+TheFree-Man@users.noreply.github.com>
2020-04-29 21:48:35 +0100

    Quicksave

--- CA 4/Q1/Program.cs
+++ CA 4/Q1/Program.cs
@@ -6,7 +6,8 @@ namespace Q1
     {
         static void Main(string[] args)
         {
-            Customer[] myCustomers = new Customer[4];
+            Customer[] myCustomers = new Customer[2];
+            TrialCustomer[] myTrialCustomers = new TrialCustomer[2];
 
             myCustomers[0] = new Customer();
             myCustomers[0].Name = "Mandy";
@@ -14,6 +15,33 @@ namespace Q1
 
             myCustomers[1] = new TrialCustomer("Jimmy", 500, 1000);
             Console.WriteLine(myCustomers[1].ToString());
+
+            myTrialCustomers[0] = new TrialCustomer("Anastasia", 500, 1000);
+            Console.WriteLine(myTrialCustomers[0].ToString());
+
+            myTrialCustomers[1] = new TrialCustomer("Maximilian", 0, 500);
+            Console.WriteLine(myTrialCustomers[1].ToString());
+            bool isValid = myTrialCustomers[1].AddCharge(700);
+            if (isValid == true)
+            {
+                Console.WriteLine(myTrialCustomers[1].ToString());
+            }
+            else
+            {
+                Console.WriteLine("\nInsufficient credit");
+            }
+
+            try
+            {
+                Customer[] myCustomersCSV = new Customer[5];
+                FileStream fs = new FileStream(@"../../customers.txt", FileMode.Open, FileAccess.Read);
+
+            }
+            catch (Exception myError)
+            {
+
+                Console.WriteLine(myError); ;
+            }
         }
     }
 }
